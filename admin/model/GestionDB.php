56a4d239cbaf0ae5a11e696614be2f6096f793a1
Amine <91972028+Amine845@users.noreply.github.com>
2025-10-12 15:26:25 +0200

    Add files via upload

--- admin/model/GestionDB.php
+++ admin/model/GestionDB.php
@@ -1,1 +1,35 @@
+<?php
+/**
+ * Fichier : gestionDB.php
+ *
+ * Classe de base pour gérer la connexion et l'accès à la base de données via PDO.
+ *
+ * @author: EL GANDOUZ Amine
+ *
+ * @method connexion() : Retourne une instance PDO
+ * @method deconnexion() : (à implémenter) Gère la fermeture de la connexion si besoin
+ * ||
+ * Erreur : Affiche un message si la connexion échoue.
+ */
 
+
+namespace bd;
+use PDOException;
+
+include('config.php');
+
+class GestionDB{
+    public $pdo;
+
+    public function connexion(){
+        try{
+            $this->pdo = new \PDO("pgsql:host=".SERVERNAME.";port=".PORT.";dbname=".DBNAME, USERNAME, PASSWORD);
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
+
+    public function deconnexion(){
+        $this->pdo = null; // Ferme la connexion en supprimant l'objet PDO
+    }
+}
