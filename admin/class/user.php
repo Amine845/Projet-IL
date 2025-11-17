e67f31d6fc397e0d8db2facae4a781579286dbec
Amine <91972028+Amine845@users.noreply.github.com>
2025-11-17 11:41:19 +0000

    classes admin

--- admin/class/user.php
+++ admin/class/user.php
@@ -1,5 +1,44 @@
 <?php
 /**
- * classe user
- * 
+ * Fichier : user.php
+ *
+ * Représente un utilisateur avec ses attributs (user_id, username, mot de passe, rôle).
+ *
+ * @author: EL GANDOUZ Amine
+ *
+ * @property int $user_id
+ * @property string $username
+ * @property string $password
+ * @property string $role
  */
+
+
+namespace classe;
+
+class user{
+    public $username;
+    public $password;
+    public $user_id;
+
+    public function __construct($username = '', $password = '') {
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function getUsername() {
+        return $this->username;
+    }
+
+    public function getId() {
+        return $this->user_id;
+    }
+
+    public function setPassword($val) {
+        $this->password = $val;
+    }
+
+
+}
+
+
+
