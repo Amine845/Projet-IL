64cc7755868ceed763a5289584bf58acae7ce35f
Suufiane3 <96297869+Suufiane3@users.noreply.github.com>
2025-12-03 09:29:38 +0000

    video sync

--- control/server.js
+++ control/server.js
@@ -8,17 +8,12 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// --- GESTION DES CHEMINS ---
+// --- CHEMINS ---
 const cheminRacine = path.join(__dirname, '../');
 app.use(express.static(cheminRacine));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(cheminRacine, 'index.html'));
-});
-
-app.get('/room', (req, res) => {
-    res.sendFile(path.join(cheminRacine, 'room.html'));
-});
+app.get('/', (req, res) => res.sendFile(path.join(cheminRacine, 'index.html')));
+app.get('/room', (req, res) => res.sendFile(path.join(cheminRacine, 'room.html')));
 
 // --- VARIABLES ---
 let rooms = {}; 
@@ -34,11 +29,17 @@ io.on('connection', (socket) => {
         socket.emit('room_created', code);
     });
 
+    socket.on('change_video', (data) => {
+        // data = { roomCode, videoId }
+        io.to(data.roomCode).emit('load_video', data.videoId);
+    });
+
     socket.on('join_room', (data) => {
         const { roomCode, username } = data;
 
         if (!rooms[roomCode]) {
-            rooms[roomCode] = { users: [] };
+            // Ajout du champ 'controller' pour savoir qui dirige la vidéo
+            rooms[roomCode] = { users: [], controller: null };
         }
 
         rooms[roomCode].users.push({ id: socket.id, username: username });
@@ -47,7 +48,12 @@ io.on('connection', (socket) => {
         // Mise à jour liste users
         io.to(roomCode).emit('update_users', rooms[roomCode].users);
         
-        // Message système : Entrée
+        // Dire au nouveau qui est le contrôleur actuel
+        if (rooms[roomCode].controller) {
+            socket.emit('update_controller', rooms[roomCode].controller);
+        }
+        
+        // Message système
         socket.to(roomCode).emit('receive_message', {
             username: 'Système',
             text: `${username} a rejoint le salon.`,
@@ -56,16 +62,39 @@ io.on('connection', (socket) => {
     });
 
     socket.on('send_message', (data) => {
-        // On renvoie le message à tout le monde dans la salle
         io.to(data.roomCode).emit('receive_message', data);
     });
 
-    // --- NOUVEAU : Gestion du "En train d'écrire..." ---
     socket.on('typing', (data) => {
-        // On envoie l'info à tout le monde sauf à celui qui écrit
         socket.to(data.roomCode).emit('user_typing', data.username);
     });
 
+    // --- NOUVEAU : GESTION VIDÉO SYNCHRO ---
+
+    // 1. Un utilisateur demande à prendre le contrôle
+    socket.on('claim_control', (data) => {
+        const { roomCode, username } = data;
+        if (rooms[roomCode]) {
+            rooms[roomCode].controller = username;
+            // On prévient tout le monde
+            io.to(roomCode).emit('update_controller', username);
+            
+            io.to(roomCode).emit('receive_message', {
+                username: 'Système',
+                text: `${username} a pris le contrôle de la vidéo.`,
+                isSystem: true
+            });
+        }
+    });
+
+    // 2. Actions vidéo (Lecture, Pause, Seek)
+    socket.on('video_action', (data) => {
+        // data = { roomCode, type: 'play'|'pause'|'seek', currentTime }
+        // On renvoie l'action à TOUS les autres sauf l'émetteur
+        socket.to(data.roomCode).emit('sync_video', data);
+    });
+
+    // --- DÉCONNEXION ---
     socket.on('disconnect', () => {
         for (const code in rooms) {
             const userIndex = rooms[code].users.findIndex(user => user.id === socket.id);
@@ -73,13 +102,14 @@ io.on('connection', (socket) => {
             if (userIndex !== -1) {
                 const username = rooms[code].users[userIndex].username;
                 
-                // Retirer l'utilisateur
-                rooms[code].users.splice(userIndex, 1);
-                
-                // Mettre à jour la liste
-                io.to(code).emit('update_users', rooms[code].users);
+                // Si c'était le contrôleur, on réinitialise
+                if (rooms[code].controller === username) {
+                    rooms[code].controller = null;
+                    io.to(code).emit('update_controller', 'Personne');
+                }
 
-                // Message système : Sortie (Requis par le CDC)
+                rooms[code].users.splice(userIndex, 1);
+                io.to(code).emit('update_users', rooms[code].users);
                 io.to(code).emit('receive_message', {
                     username: 'Système',
                     text: `${username} a quitté le salon.`,
